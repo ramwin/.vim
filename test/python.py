e1dd9b65b3eca644bc99849e6473e4c178157608
Xiang Wang <ramwin@qq.com>
2022-06-28 23:23:48 +0800

    jedi

--- test/python.py
+++
@@ -1,7 +0,0 @@
-def main():
-    pass
-
-
-# jA↵ 应该出现自动缩进
- 
-main(
---
+++ test/test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Xiang Wang <ramwin@qq.com>
+
+
+from pathlib import Path
+import os
+
+import re
+
+def main():
+    pass
+
+
+# jA↵ 应该出现自动缩进
+ 
+main()
